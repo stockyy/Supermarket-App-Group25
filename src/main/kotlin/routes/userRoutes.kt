81cceeedd653d021ef02cccbb48aebaf838ac437
rafi kyum <kyumrafi@gmail.com>
2026-04-02 23:52:06 +0100

    Exisiting user rouets have been properly modualarised

---
+++ src/main/kotlin/routes/userRoutes.kt
@@ -0,0 +1,17 @@
+package com.supermarket.routes
+
+import com.supermarket.database.StringRepository
+import io.ktor.http.ContentType
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.Route
+import io.ktor.server.routing.get
+import io.ktor.server.routing.route
+
+fun Route.userRouting() {
+    route("/print-all-workers") {
+        get {
+            val userText = StringRepository.getAllWorkersString()
+            call.respondText(userText, ContentType.Text.Plain)
+        }
+    }
+}
