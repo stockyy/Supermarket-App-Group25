e8ea98b41a9a4fd2b85185b997b60eed8219bc02
stockyy <fnnb5132@leeds.ac.uk>
2026-04-08 18:06:15 +0100

    added ability to request a specific number of products from the API

--- src/main/kotlin/database/ApiQuerying.kt
+++ src/main/kotlin/database/ApiQuerying.kt
@@ -8,6 +8,7 @@ import kotlinx.serialization.json.Json
 import io.ktor.client.call.*
 import io.ktor.client.request.*
 import io.ktor.http.isSuccess
+import kotlin.collections.*
 
 /**
  * Create a single 'HttpClient' engine for the whole app to use.
@@ -21,40 +22,54 @@ val httpClient = HttpClient(CIO) {
 }
 
 // suspend function so a thread doesn't have to wait for the query to be returned
-suspend fun getGroceryData(): List<OffProduct> {
+// finds 500 products by default
+suspend fun getGroceryData(targetAmount: Int = 500): List<OffProduct> {
+    val allFilteredProducts = mutableListOf<OffProduct>()
+    var page = 1
+
+    println("Connecting to Open Food Facts API to fetch ${targetAmount} products...")
+
     return try {
-        println("Connecting to Open Food Facts API...")
+        // keep looping until wee have hit our target number of products
+        while (allFilteredProducts.size < targetAmount) {
 
-        val url =
-            "https://world.openfoodfacts.org/api/v2/search?categories_tags_en=groceries&page_size=100&fields=code,product_name,categories,image_url&sort_by=unique_scans_n"
+            val url =
+                "https://world.openfoodfacts.org/api/v2/search?categories_tags_en=groceries&page_size=250&page=$page&fields=code,product_name,categories,image_url&sort_by=unique_scans_n"
 
-        // send the request to the API with headers to identify our app so we don't get blocked
-        val httpResponse = httpClient.get(url) {
-            header("User-Agent", "Supermarket App - Kotlin Student Project")
-        }
-
-        // if request was a success, then get response body and filter out products without enough data
-        // if request fails, print error message and return empty list
-        if (httpResponse.status.isSuccess()) {
-            val response: OpenFoodFactsResponse = httpResponse.body()
-
-            // filter out products without enough data
-            val filteredProducts = response.products.filter {
-                !it.productName.isNullOrBlank() &&
-                        !it.categories.isNullOrBlank() &&
-                        !it.code.isNullOrBlank() &&
-                        !it.imageUrl.isNullOrBlank()
+            // send the request to the API with headers to identify our app so we don't get blocked
+            val httpResponse = httpClient.get(url) {
+                header("User-Agent", "Supermarket App - Kotlin Student Project")
             }
 
-            filteredProducts
+            // if request was a success, then get response body and filter out products without enough data
+            // if request fails, print error message and return empty list
+            if (httpResponse.status.isSuccess()) {
+                val response: OpenFoodFactsResponse = httpResponse.body()
 
-        } else {
-            println("Failed to fetch data from Open Food Facts API. Status code: ${httpResponse.status}")
-            emptyList() // return an empty list if the request was not successful
+                // filter out products without enough data
+                val filteredProductsBatch = response.products.filter {
+                    !it.productName.isNullOrBlank() &&
+                            !it.categories.isNullOrBlank() &&
+                            !it.code.isNullOrBlank() &&
+                            !it.imageUrl.isNullOrBlank()
+                }
+
+                allFilteredProducts.addAll(filteredProductsBatch)
+                println("Page ${1} processed, ${filteredProductsBatch.size} valid products found. Total valid products so far: ${allFilteredProducts.size}")
+                page++
+
+            } else {
+                println("Failed to fetch data from Open Food Facts API. Status code: ${httpResponse.status}")
+                return emptyList() // return an empty list if the request was not successful
+                break
+            }
+
         }
+        println("Succeessfully collected ${allFilteredProducts.size} valid products.")
+        return allFilteredProducts.take(targetAmount)
     }
 
-    // return an empty list if there was an error connecting to the API, and print the error message to the terminal
+// return an empty list if there was an error connecting to the API, and print the error message to the terminal
     catch (e: Exception) {
         println("Error connecting to Open Food Facts API: ${e.message}")
         emptyList()
