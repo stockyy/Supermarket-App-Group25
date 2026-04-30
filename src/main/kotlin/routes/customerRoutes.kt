9c38cda348dd4d8a4662223f24d8d07e65fe42eb
rafi kyum <kyumrafi@gmail.com>
2026-04-30 13:53:37 +0100

    basket page frontend completed

--- src/main/kotlin/routes/customerRoutes.kt
+++ src/main/kotlin/routes/customerRoutes.kt
@@ -158,6 +158,18 @@ fun Route.customerRoutes() {
             }
         }
 
+        get("/basket") {
+            val html = call.application.javaClass
+                .getResource("/static/views/customer/basket.html")
+                ?.readText()
+
+            if (html != null) {
+                call.respondText(html, ContentType.Text.Html)
+            } else {
+                call.respondText("Login page not found", status = HttpStatusCode.NotFound)
+            }
+        }
+
 
         post("/logout") {
             // logoutCustomer
